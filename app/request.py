c25a6b3bd5848dac6bbd1210b7ecceef6fb67a0b
NDOLIC <tblaguese1@gmail.com>
2019-02-18 16:55:36 +0200

    changes in views

--- app/request.py
+++ app/request.py
@@ -11,21 +11,57 @@ api_key = app.config['NEWS_API_KEY']
 base_url = app.config["NEWS_API_BASE_URL"]
 
 
+# def get_sources(category):
+#     '''
+#     Function that gets the json response to our url request
+#     '''
+#    
+
+#     with urllib.request.urlopen(get_sources_url) as url:
+#         get_sources_data = url.read()
+#         get_sources_response = json.loads(get_sources_data)
+
+#         sources_sources = None
+
+#         if get_sources_response['sources']:
+#             sources_sources_list = get_sources_response['sources']
+#             sources_sources = process_sources(sources_sources_list)
+
+
+#     return sources_sources
 def get_sources(category):
     '''
     Function that gets the json response to our url request
     '''
     get_sources_url = base_url.format(category,api_key)
-
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
+        get_sources_response= json.loads(get_sources_data)
 
-        sources_sources = None
-
+        sources_results = None
         if get_sources_response['sources']:
-            sources_sources_list = get_sourcess_response['sources']
-            sources_sources = process_sources(sources_sources_list)
+            sources_list = get_sources_response['sources']
+            sources = process_sources(sources_list)
+    return sources_results
 
+def process_sources(sources_list):
+    '''
+    Function that processes the news sources and transform them to a list of Objects
+    Args:
+        sources_list: A list of dictionaries that contain news details
+    Returns :
+        news_sources: A list of sources objects
+    '''
+    news_sources = []
+    for news_source in sources_list:
+        source_id = news_source.get('id')
+        source_name = news_source.get('name')
+        source_url= news_source.get('url')
+        source_description=news_source.get('description')
+        source_category= news_source.get('category')
+        source_language=news_source.get('language')
 
-    return sources_sources
+        news_source = NewsSource(source_id,source_name,source_description,source_url,source_category,source_language)
+        news_sources.append(news_source)
+
+    return news_sources
--- app/views.py
+++ app/views.py
@@ -4,16 +4,20 @@ from app import app
 from .request import get_sources
 
 
-@main.route('/')
+@app.route('/')
 
 def index():
 
     '''
     This view function returns the index page and its data
     '''
-
-    news_sources=get_sources()
+todays_highlights = get_news("general")
+    todays_weather = get_news('weather')
+    todays_sports = get_news('sports')
+    news_sources = get_sources('sources')
     title = 'Home-Welcome to the Breaking news updates'
-    return render_template('index.html',title=title, sources=news_sources)
+    return render_template('index.html', title=title, general=todays_highlights ,weather=todays_weather,sports=todays_sports)
+    
+    # return render_template('index.html',title=title, sources=news_sources)
 
 
