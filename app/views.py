f81fb30a3c279057c853d97ba981fa7c7811078c
NDOLIC <tblaguese1@gmail.com>
2019-02-19 08:19:56 +0200

    inserting column in index.html

--- app/views.py
+++ app/views.py
@@ -15,9 +15,13 @@ def index():
     general_sources = get_sources('general')
     business_sources = get_sources('business')
     sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    sciences_sources = get_sources('science')
+    entertainment_sources = get_sources('entertainment')
+    health_sources = get_sources('health')
 
     title = 'Home-Welcome to the Breaking news updates'
-    return render_template('index.html', title = title,general = general_sources, business= business_sources,sports=sports_sources)
+    return render_template('index.html', title = title,general = general_sources, business= business_sources,sports=sports_sources,technology=technology_sources,science=sciences_sources,entertainment=entertainment_sources,health=health_sources)
     # return render_template('index.html', title=title, general=todays_highlights ,weather=todays_weather,sports=todays_sports)
     
     # return render_template('index.html',title=title, sources=news_sources)
